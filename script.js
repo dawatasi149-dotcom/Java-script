1fe8de132c305d847bbe8afcf48c573e305bb9f1
Dawa tashi Sherpa <dawatasi149@gmail.com>
2025-12-30 17:56:23 +0545

    create an object of own profile

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const product = {
-    title:"pen",
-    fullname: "Cello",
-    price: 20,
-    writeable: true,
-    color: blck,
-    rating: 5,
+const profile = {
+    fullname: "Dawa tashi Sherpa",
+    followers: 170,
+    following: 80,
+    post: 0,
+    
 }
-console.log(Pen);
+console.log(profile);
