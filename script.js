53b351f35ace6ae9dc160687b5d1a141d2cdbe9a
Dawa tashi Sherpa <dawatasi149@gmail.com>
2026-01-02 19:01:39 +0545

    learn loops and string

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-//create prompt
-let num = prompt("Enter your grade ");
-if (num<30) {
-    console.log("you are fail");
-} else if (num<40){
-    console.log("B");
+let gamenum = 25;
+let usernum = prompt("enter your gess number ")
+
+
+while (usernum != gamenum) {
+    usernum = prompt("You enter wrong number. Please gess again  ");
 }
+console.log("Congratulation");
