4cb71d8af1321db3481ba053036e594e205ab2c0
Dawa tashi Sherpa <dawatasi149@gmail.com>
2026-01-03 09:46:52 +0545

    learn Arry in js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-let gamenum = 25;
-let usernum = prompt("enter your gess number ")
-
-
-while (usernum != gamenum) {
-    usernum = prompt("You enter wrong number. Please gess again  ");
+//ARRAYS IN JS 
+let price = [250, 645, 300, 900, 50];
+let finalprice = 0;
+for (let i of price) {
+    console.log(` Price befofe discount ${i}`)
+ 
 }
-console.log("Congratulation");
+
+for (let i of price) {
+    finalprice = i - i * 0.1;
+    console.log(`Price after discount ${finalprice}`);
+}
