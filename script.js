a112e0864fe5e97c73a417ee15ab863f48f0505d
Dawa tashi Sherpa <dawatasi149@gmail.com>
2026-01-04 15:38:05 +0545

    learn advance arry in js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-//ARRAYS IN JS 
-let price = [250, 645, 300, 900, 50];
-let finalprice = 0;
-for (let i of price) {
-    console.log(` Price befofe discount ${i}`)
- 
-}
-
-for (let i of price) {
-    finalprice = i - i * 0.1;
-    console.log(`Price after discount ${finalprice}`);
-}
+// QUESTION 
+let company = ["Bloomberg", "Microshoft", "uber", "google", "IBM", "Netflix"];
+company.shift();
+console.log(company);
+//to remove uber and add ola in its place 
+company.splice(2, 1, "ola");
+console.log(company);
+company.push("Amazon");
+console.log(company);
